0bbb98de3e416207fc4968e51ecb9a156c7d8037
0sayo0 <jonathansme01@gmail.com>
2023-05-31 20:32:00 -0600

    Se agrega la 2da pre-entrega

--- js/app.js
+++ js/app.js
@@ -1,31 +1,48 @@
 
-function simularInteractivo() {
-    let nombre = prompt("¿Cuál es tu nombre?");
-    let edad = parseInt(prompt("¿Cuál es tu edad?"));
-
-    function mostrarMensaje() {
-        let mensaje = "¡Hola, " + nombre + "! En 10 años tendrás " + (edad + 10) + " años.\n";
-
-        for (let i = 1; i <= 5; i++) {
-            let num = parseInt(prompt("Ingresa un número #" + i + ":"));
-            mensaje += "Número #" + i + ": " + num + "\n";
-        }
-
-        alert(mensaje);
-    }
-
-    if (edad < 18) {
-        alert("Lo siento, " + nombre + ", eres menor de edad y no puedes continuar.");
-    } else {
-        mostrarMensaje();
-    }
-
-    function saludar() {
-        alert("Hasta Pronto, " + nombre + "!");
-    }
-
-    saludar();
-    }
+// Objeto para almacenar y mostrar la información
+const simulador = {
+    numeros: [],
+    resultado: 0,
+    operacion: '',
   
-simularInteractivo();
+    // Función para realizar el cálculo
+    calcular: function(operacion) {
+      // Reiniciar el arreglo de números
+      this.numeros = [];
+  
+      // Pedir entrada al usuario
+      let cantidadNumeros = parseInt(prompt('Ingresa la cantidad de números:'));
+  
+      // Pedir los números al usuario
+      for (let i = 1; i <= cantidadNumeros; i++) {
+        let numero = parseInt(prompt('Ingresa el número #' + i + ':'));
+        this.numeros.push(numero);
+      }
+  
+      // Realizar el cálculo según la operación
+      if (operacion === 'suma') {
+        this.resultado = this.sumar();
+        this.operacion = 'suma';
+      } else if (operacion === 'resta') {
+        this.resultado = this.restar();
+        this.operacion = 'resta';
+      }
+  
+      // Mostrar el resultado
+      alert('El resultado de la ' + this.operacion + ' es: ' + this.resultado);
+    },
+  
+    // Función para sumar los números del arreglo
+    sumar: function() {
+      return this.numeros.reduce((total, numero) => total + numero, 0);
+    },
+  
+    // Función para restar los números del arreglo
+    restar: function() {
+      return this.numeros.reduce((total, numero) => total - numero);
+    }
+  };
+  
+  // Llamar a la función del simulador con la operación deseada (puede ser 'suma' o 'resta')
+  simulador.calcular('suma');
   
