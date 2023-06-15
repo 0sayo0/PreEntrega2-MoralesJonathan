6ecaff53719ab139f47a11f9c91a2a7096ddbe0e
0sayo0 <jonathansme01@gmail.com>
2023-06-14 20:15:07 -0600

    Se agrega la PreEntrega 3

--- js/app.js
+++ js/app.js
@@ -1,48 +1,131 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+let articulosCarrito = [];
 
-// Objeto para almacenar y mostrar la información
-const simulador = {
-    numeros: [],
-    resultado: 0,
-    operacion: '',
-  
-    // Función para realizar el cálculo
-    calcular: function(operacion) {
-      // Reiniciar el arreglo de números
-      this.numeros = [];
-  
-      // Pedir entrada al usuario
-      let cantidadNumeros = parseInt(prompt('Ingresa la cantidad de números:'));
-  
-      // Pedir los números al usuario
-      for (let i = 1; i <= cantidadNumeros; i++) {
-        let numero = parseInt(prompt('Ingresa el número #' + i + ':'));
-        this.numeros.push(numero);
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+     // Dispara cuando se presiona "Agregar Carrito"
+     listaCursos.addEventListener('click', agregarCurso);
+
+     // Cuando se elimina un curso del carrito
+     carrito.addEventListener('click', eliminarCurso);
+
+     // Al Vaciar el carrito
+     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+
+     // NUEVO: Contenido cargado
+     document.addEventListener('DOMContentLoaded', () => {
+          articulosCarrito = JSON.parse( localStorage.getItem('carrito') ) || []  ;
+          // console.log(articulosCarrito);
+          carritoHTML();
+     });
+}
+
+
+// Función que añade el curso al carrito
+function agregarCurso(e) {
+     e.preventDefault();
+     // Delegation para agregar-carrito
+     if(e.target.classList.contains('agregar-carrito')) {
+          const curso = e.target.parentElement.parentElement;
+          // Enviamos el curso seleccionado para tomar sus datos
+          leerDatosCurso(curso);
+     }
+}
+
+// Lee los datos del curso
+function leerDatosCurso(curso) {
+     const infoCurso = {
+          imagen: curso.querySelector('img').src,
+          titulo: curso.querySelector('h4').textContent,
+          precio: curso.querySelector('.precio span').textContent,
+          id: curso.querySelector('a').getAttribute('data-id'), 
+          cantidad: 1
+     }
+
+
+     if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
+          const cursos = articulosCarrito.map( curso => {
+               if( curso.id === infoCurso.id ) {
+                    let cantidad = parseInt(curso.cantidad);
+                    cantidad++
+                    curso.cantidad =  cantidad;
+                    return curso;
+               } else {
+                    return curso;
+               }
+          })
+          articulosCarrito = [...cursos];
+     }  else {
+          articulosCarrito = [...articulosCarrito, infoCurso];
+     }
+
+     console.log(articulosCarrito)
+
+     
+
+     // console.log(articulosCarrito)
+     carritoHTML();
+}
+
+// Elimina el curso del carrito en el DOM
+function eliminarCurso(e) {
+     e.preventDefault();
+     if(e.target.classList.contains('borrar-curso') ) {
+          // e.target.parentElement.parentElement.remove();
+          const curso = e.target.parentElement.parentElement;
+          const cursoId = curso.querySelector('a').getAttribute('data-id');
+          
+          // Eliminar del arreglo del carrito
+          articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+          carritoHTML();
+     }
+}
+
+
+// Muestra el curso seleccionado en el Carrito
+function carritoHTML() {
+
+     vaciarCarrito();
+
+     articulosCarrito.forEach(curso => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+               <td>  
+                    <img src="${curso.imagen}" width=100>
+               </td>
+               <td>${curso.titulo}</td>
+               <td>${curso.precio}</td>
+               <td>${curso.cantidad} </td>
+               <td>
+                    <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+               </td>
+          `;
+          contenedorCarrito.appendChild(row);
+     });
+
+     // NUEVO:
+     sincronizarStorage();
+
+}
+
+
+// NUEVO: 
+function sincronizarStorage() {
+     localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
+}
+
+// Elimina los cursos del carrito en el DOM
+function vaciarCarrito() {
+     // forma rapida (recomendada)
+     while(contenedorCarrito.firstChild) {
+          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
       }
-  
-      // Realizar el cálculo según la operación
-      if (operacion === 'suma') {
-        this.resultado = this.sumar();
-        this.operacion = 'suma';
-      } else if (operacion === 'resta') {
-        this.resultado = this.restar();
-        this.operacion = 'resta';
-      }
-  
-      // Mostrar el resultado
-      alert('El resultado de la ' + this.operacion + ' es: ' + this.resultado);
-    },
-  
-    // Función para sumar los números del arreglo
-    sumar: function() {
-      return this.numeros.reduce((total, numero) => total + numero, 0);
-    },
-  
-    // Función para restar los números del arreglo
-    restar: function() {
-      return this.numeros.reduce((total, numero) => total - numero);
-    }
-  };
-  
-  // Llamar a la función del simulador con la operación deseada (puede ser 'suma' o 'resta')
-  simulador.calcular('suma');
-  
+}
